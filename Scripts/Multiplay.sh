c29fae13b641325cea87aae8a1da6b109871b343
David <davidjhoenisch@protonmail.com>
2020-08-28 22:45:42 -0600

    some improvments
    these improvements make the multiplay script.  It does not do anything new at the core.  However, it does let the use know what is going on in more detail and offers the options for them to exit the script without using ctrl+c if they realize that they don't actually want to proceed.

--- Scripts/Multiplay.sh
+++ Scripts/Multiplay.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-ansible-playbook filebeat_playbook.yml
-ansible-playbook metricbeat_playbook.yml
-echo "elk stack install complete!"
+echo "runnning the multiplay script"
+sleep 1s
+echo " "
+echo "after completing, filebeat & metricbeat will be installed"
+echo " "
+sleep 1s
+read -p "do you wish to continue(yes/no)?: " answer
+if [[ $answer == "no" ]];
+  then
+    echo "exiting..."
+    sleep 1s
+    exit
+  fi
+ansible-playbook filebeat_playbook.yml || echo "error occured"
+ansible-playbook metricbeat_playbook.yml || echo "error occured"
+echo " "
+echo "go to the web server address to verify that filebeat and metricbeat are working correctly"
+echo " "
+sleep 1s
+echo "exiting..."
